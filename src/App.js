388d16e6b06fc038ef875597b6ffb61b420561de
Bahar Sarlak <bsarlak98@gmail.com>
2021-12-15 14:21:00 -0500

    navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 
+import svg from './images/female-developer-illustration/female developer 1---.svg'
+
 function App() {
   return (
-    <div className="container mx-auto ">
-      <h1 className="bg-blue-500">portfolio page</h1> 
+    <div className="bg-gradient-to-b from-gray-500 to-gray-800 px-20 ">
+      <div className='py-8 overflow-auto'>
+        <img src={svg} alt="svg" width="30%" className='float-right object-fill pr-20'/>
+        <h1 className="w-2/3 text-indigo-400 text-6xl py-5 px-8">Welcome to my page</h1>
+        <h4 className="text-gray-100 w-1/2 text-3xl py-9 px-10"> My name is Bahar <br/> and I am a web developer!</h4>
+      </div>
+      <div className='pt-20 w-1/2 mx-auto pb-10 flex flex-row gap-4 text-gray-100'>
+        <button type="button" className='btn-indigo'>
+          <a href="#about">About Me</a>  
+        </button> 
+        <button type="button" className='btn-indigo'>
+          <a href="#about">My Work</a>  
+        </button>
+        <button type="button" className='btn-indigo'>
+          <a href="#about">Contact</a>  
+        </button>
+      </div>
+      <div>
+        <h2>About Me</h2>
+        <p>My name is Bahar Sarlak.</p>
+      </div>
     </div>
   );
 }
